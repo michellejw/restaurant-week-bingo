4a121266dd4d3abe05e8bf54a52dcff46e0b67fe
Michelle Weirathmueller <michellejw@gmail.com>
2025-01-12 14:51:16 -0500

    Add test restaurant data migration

---
+++ src/db/migrations/005_add_test_restaurants.sql
@@ -0,0 +1,24 @@
+-- Insert test restaurant data
+INSERT INTO restaurants (name, address, website_url, latitude, longitude)
+VALUES 
+    (
+        'Michael''s Seafood',
+        '1206 N Lake Park Blvd, Carolina Beach, NC 28428',
+        'https://www.michaelsseafood.com',
+        34.0494,
+        -77.9006
+    ),
+    (
+        'Malama Cafe',
+        '1000 S Lake Park Blvd, Carolina Beach, NC 28428',
+        'https://www.malamacafe.com',
+        34.0362,
+        -77.9078
+    ),
+    (
+        'Soul Flavor',
+        '716 N Lake Park Blvd, Carolina Beach, NC 28428',
+        'https://www.soulflavor.com',
+        34.0355,
+        -77.9094
+    ); 
